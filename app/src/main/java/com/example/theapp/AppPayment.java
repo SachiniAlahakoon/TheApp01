f88f3713a2836cc221deb92a8d8345d3c2470ce0
DELL <sachinialahakoon327@gmail.com>
2023-06-03 17:45:09 +0530

    Easy Travel The App for the EEI4369 Course error fixed 01

--- app/src/main/java/com/example/theapp/AppPayment.java
+++ app/src/main/java/com/example/theapp/AppPayment.java
@@ -1,27 +1,19 @@
 package com.example.theapp;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AppPayment extends AppCompatActivity {
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_payment);
 
-        button = (Button) findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                OpenWelcomePage();
-            }
-
-        });
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(v -> OpenWelcomePage());
     }
 
     private void OpenWelcomePage() {
